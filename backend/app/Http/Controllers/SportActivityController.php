0a27606d6fa92eaebd8ff314c02ebece1b5331c4
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-03-26 17:38:19 -0300

    feat: create basic controller for sport activity

---
+++ backend/app/Http/Controllers/SportActivityController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SportActivityController extends Controller
+{
+    /**
+     * Return all sport activities
+     *
+     * @return void
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Create a new sport acitivty
+     *
+     * @return void
+     */
+    public function create()
+    {
+    }
+}
